6adf0c6fa6375bdc7c2558c0270609235bd9957b
Russ485 <kazyonnylawyer@gmail.com>
2024-09-24 18:06:40 +0200

    hoping thats all

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "CaseCobra <kazyonnylawyer@gmail.com>",
+        from: "CaseCobra <noreply@casecobra.example.com>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
